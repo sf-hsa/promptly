7c9026dbf414dc4b9f42ab90c47a86581e8d2d62
Kevin Ho <kevin.ho@sfgov.org>
2015-05-08 15:53:12 -0700

    add double quotes and comments

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,59 @@
+=begin
+	To run import_group manually, at the Promptly root, run each line below
+	  rake "import_group[dc_waived_calfresh_english,dc_waived_calfresh_english, 1]"
+	  rake "import_group[dc_waived_calfresh_spanish,dc_waived_calfresh_spanish, 1]"
+	  rake "import_group[dc_waived_calfresh_chinese,dc_waived_calfresh_chinese, 1]"
+	  rake "import_group[dc_waived_calfresh_vietnamese,dc_waived_calfresh_vietnamese, 1]"
+	  rake "import_group[dc_waived_calfresh_russian,dc_waived_calfresh_russian, 1]"
+	  rake "import_group[dc_waived_calfresh_tagalog,dc_waived_calfresh_tagalog, 1]"
+	  rake "import_group[dc_waived_calfresh_other,dc_waived_calfresh_other, 1]"
+	  rake "import_group[waived_cases,all_waived_cases, 1]"
+	  rake "import_group[dc_waived_medical,dc_waived_medical, 21]"
+	  rake "import_group[waived_cases,waived_cases, 21]"
+	  rake "import_group[waived_medical,waived_medical, 21]"
+	  rake "import_group[waived_medical_chinese,waived_medical_chinese, 21]"
+	  rake "import_group[waived_medical_english,waived_medical_english, 21]"
+	  rake "import_group[waived_medical_other,waived_medical_other, 21]"
+	  rake "import_group[waived_medical_russian,waived_medical_russian, 21]"
+	  rake "import_group[waived_medical_spanish,waived_medical_spanish, 21]"
+	  rake "import_group[waived_medical_tagalog,waived_medical_tagalog, 21]"
+	  rake "import_group[waived_medical_vietnamese,waived_medical_vietnamese, 21]"
+	
+	Inside the bracket[], 1st parameter is table name, 2nd parameter is group name, 3rd parameter is organization id.
+	To check if any schedule job, at Promptly root, type "crontab -l".
+	You can also run the schedule, copy from screen, you have something like below
+
+0 10 * * * /bin/bash -l -c 'cd /home/likewise-open/HSA/promptly && RAILS_ENV=production bundle exec rake "import_group[dc_waived_calfresh_english,dc_waived_calfresh_english, 1]" --silent >> log/cron.log 2>&1'
+0 10 * * * /bin/bash -l -c 'cd /home/likewise-open/HSA/promptly && RAILS_ENV=production bundle exec rake "import_group[dc_waived_calfresh_spanish,dc_waived_calfresh_spanish, 1]" --silent >> log/cron.log 2>&1'
+...
+	Just run below at Promptly root, to test it out
+		/bin/bash -l -c 'cd /home/likewise-open/HSA/promptly && RAILS_ENV=production bundle exec rake "import_group[dc_waived_calfresh_english,dc_waived_calfresh_english, 1]"'
+		
+	To update the schedule after modify this file, type below at Promptly root
+	whenever --update-crontab
+=end
+
 set :output, 'log/cron.log'
 
 every 1.day, :at => '10:00 am' do
-  rake "import_group[dc_waived_calfresh_english,dc_waived_calfresh_english, 1]"
-  rake "import_group[dc_waived_calfresh_spanish,dc_waived_calfresh_spanish, 1]"
-  rake "import_group[dc_waived_calfresh_chinese,dc_waived_calfresh_chinese, 1]"
-  rake "import_group[dc_waived_calfresh_vietnamese,dc_waived_calfresh_vietnamese, 1]"
-  rake "import_group[dc_waived_calfresh_russian,dc_waived_calfresh_russian, 1]"
-  rake "import_group[dc_waived_calfresh_tagalog,dc_waived_calfresh_tagalog, 1]"
-  rake "import_group[dc_waived_calfresh_other,dc_waived_calfresh_other, 1]"
-  rake "import_group[waived_cases,all_waived_cases, 1]"
-  rake "import_group[dc_waived_medical,dc_waived_medical, 21]"
-  rake "import_group[waived_cases,waived_cases, 21]"
-  rake "import_group[waived_medical,waived_medical, 21]"
-  rake "import_group[waived_medical_chinese,waived_medical_chinese, 21]"
-  rake "import_group[waived_medical_english,waived_medical_english, 21]"
-  rake "import_group[waived_medical_other,waived_medical_other, 21]"
-  rake "import_group[waived_medical_russian,waived_medical_russian, 21]"
-  rake "import_group[waived_medical_spanish,waived_medical_spanish, 21]"
-  rake "import_group[waived_medical_tagalog,waived_medical_tagalog, 21]"
-  rake "import_group[waived_medical_vietnamese,waived_medical_vietnamese, 21]"
+  rake "\"import_group[dc_waived_calfresh_english,dc_waived_calfresh_english, 1]\""
+  rake "\"import_group[dc_waived_calfresh_spanish,dc_waived_calfresh_spanish, 1]\""
+  rake "\"import_group[dc_waived_calfresh_chinese,dc_waived_calfresh_chinese, 1]\""
+  rake "\"import_group[dc_waived_calfresh_vietnamese,dc_waived_calfresh_vietnamese, 1]\""
+  rake "\"import_group[dc_waived_calfresh_russian,dc_waived_calfresh_russian, 1]\""
+  rake "\"import_group[dc_waived_calfresh_tagalog,dc_waived_calfresh_tagalog, 1]\""
+  rake "\"import_group[dc_waived_calfresh_other,dc_waived_calfresh_other, 1]\""
+  rake "\"import_group[waived_cases,all_waived_cases, 1]\""
+  rake "\"import_group[dc_waived_medical,dc_waived_medical, 21]\""
+  rake "\"import_group[waived_cases,waived_cases, 21]\""
+  rake "\"import_group[waived_medical,waived_medical, 21]\""
+  rake "\"import_group[waived_medical_chinese,waived_medical_chinese, 21]\""
+  rake "\"import_group[waived_medical_english,waived_medical_english, 21]\""
+  rake "\"import_group[waived_medical_other,waived_medical_other, 21]\""
+  rake "\"import_group[waived_medical_russian,waived_medical_russian, 21]\""
+  rake "\"import_group[waived_medical_spanish,waived_medical_spanish, 21]\""
+  rake "\"import_group[waived_medical_tagalog,waived_medical_tagalog, 21]\""
+  rake "\"import_group[waived_medical_vietnamese,waived_medical_vietnamese, 21]\""
 end
 
 every 2.minutes do
